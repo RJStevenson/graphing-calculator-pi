5a9db1fa08a32943dd4ce77a52efdd7c0038d595
James Stevenson <james.steve.dev@gmail.com>
2020-06-18 21:00:46 +0200

    added things to the ignore file, added more classes to the python program including fractions

--- calc_software.py
+++ calc_software.py
@@ -1,3 +1,5 @@
+import math
+from fractions import Fraction
 def check_str_contains_list(ssen,slist):
     bhas = False
     ssen = str(ssen)
@@ -8,30 +10,13 @@ def check_str_contains_list(ssen,slist):
             break
     return bhas
 class formulas(object):
-
-class ops(object):#checking for the signs +-/abs etc
-    def check_for_ops(self, lnum, rnum, ssign):
-        if (check_str_contains_list(lnum, ops().lreserv()) == False) and (check_str_contains_list(rnum, ops().lreserv()) == False):
-            return [float(lnum),float(rnum)]
-        else:
-                if (check_str_contains_list(lnum, ops().lreserv()) == True):
-                    lnum = ops().numloop(lnum)
-                if (check_str_contains_list(rnum, ops().lreserv()) == True):
-                    rnum = ops().numloop(lnum)
-                return [float(lnum), float(rnum)]
-
-    def lreserv(self):
-        x = ["x", "y", "/", "+", "-", "*"]
-        return x
-    
-    def plus(self,fuck):
+    def plus(self, fuck):
         fuck = list(fuck)
         symbol = "+"
         lnum = str(fuck[0])
         rnum = str(fuck[1])
-        x =ops().check_for_ops(lnum, rnum,"+")
+        x = ops().check_for_ops(lnum, rnum, "+")
         return x[0] + x[1]
-
     def minus(self, fuck):
         fuck = list(fuck)
         symbol = "-"
@@ -48,6 +33,30 @@ class ops(object):#checking for the signs +-/abs etc
         x = ops().check_for_ops(lnum, rnum, "+")
         return x[0] * x[1]
 
+    def div(self, fuck):
+        fuck = list(fuck)
+        symbol = "-"
+        lnum = str(fuck[0])
+        rnum = str(fuck[1])
+        x = ops().check_for_ops(lnum, rnum, "+")
+        return x[0] / x[1]
+
+
+class ops(object):  #checking for the signs +-/abs etc
+    def check_for_ops(self, lnum, rnum, ssign): # for equations that have different side for example plus minus divide
+        if (check_str_contains_list(lnum, ops().lreserv()) == False) and (check_str_contains_list(rnum, ops().lreserv()) == False):
+            return [float(lnum),float(rnum)]
+        else:
+                if (check_str_contains_list(lnum, ops().lreserv()) == True):
+                    lnum = ops().numloop(lnum)
+                if (check_str_contains_list(rnum, ops().lreserv()) == True):
+                    rnum = ops().numloop(lnum)
+                return [float(lnum), float(rnum)]
+
+    def lreserv(self):
+        x = ["x", "y", "/", "+", "-", "*"]
+        return x
+    
 
     def numloop(self, ssum):
         def sides(ipos,ssum):
@@ -56,11 +65,13 @@ class ops(object):#checking for the signs +-/abs etc
             return [leftside, rightside]
         def switch(op_char, ipos, ssum):
             if(op_char=="*"):
-                return ops().multi(sides(ipos, ssum))
+                return formulas().multi(sides(ipos, ssum))
+            elif (op_char == "/"):
+                return formulas().div(sides(ipos, ssum))
             elif(op_char == "+"):
-                return ops().plus(sides(ipos,ssum))
+                return formulas().plus(sides(ipos,ssum))
             elif (op_char == "-"):
-                return ops().minus(sides(ipos, ssum))
+                return formulas().minus(sides(ipos, ssum))
 
         for k in ssum:
             if check_str_contains_list(ssum, ops().lreserv())==False:
@@ -69,6 +80,11 @@ class ops(object):#checking for the signs +-/abs etc
                   ssum = switch(k, ssum.index(k), ssum)
         return ssum
 sdata = input("enter your some")
-x = ops().numloop(sdata)
-x = float(x)
-print(str(x))
+
+if sdata.capitalize() != "FRAC":
+    x = ops().numloop(sdata)
+    x = float(x)
+    x =Fraction(x).limit_denominator(10000000)
+    print((x))
+elif sdata.capitalize() == "FRAC":
+    print(float(x))
