491093ac95c25bd02682f50e075b37facf20c6da
James Stevenson <james.steve.dev@gmail.com>
2020-06-24 13:25:15 +0200

    added in function

--- calc_software.py
+++ calc_software.py
@@ -15,14 +15,14 @@ class formulas(object):
         symbol = "+"
         lnum = str(fuck[0])
         rnum = str(fuck[1])
-        x = ops().check_for_ops(lnum, rnum, "+")
+        x = ops().check_for_ops(lnum, rnum, "")
         return x[0] + x[1]
     def minus(self, fuck):
         fuck = list(fuck)
         symbol = "-"
         lnum = str(fuck[0])
         rnum = str(fuck[1])
-        x = ops().check_for_ops(lnum, rnum, "+")
+        x = ops().check_for_ops(lnum, rnum, "")
         return x[0] - x[1]
 
     def multi(self, fuck):
@@ -30,7 +30,7 @@ class formulas(object):
         symbol = "-"
         lnum = str(fuck[0])
         rnum = str(fuck[1])
-        x = ops().check_for_ops(lnum, rnum, "+")
+        x = ops().check_for_ops(lnum, rnum, "")
         return x[0] * x[1]
 
     def div(self, fuck):
@@ -38,32 +38,66 @@ class formulas(object):
         symbol = "-"
         lnum = str(fuck[0])
         rnum = str(fuck[1])
-        x = ops().check_for_ops(lnum, rnum, "+")
+        x = ops().check_for_ops(lnum, rnum, "")
         return x[0] / x[1]
+    
+    def abs(self,fuck):
+        fuck = list(fuck)
+        symbol = "|"
+        lnum = str(fuck[0])
+        rnum = str(fuck[1])
+        inum = str(fuck[2])
+        x = ops().check_for_ops(lnum,rnum,inum)
+        x[2] = str(math.fabs(x[2]))
+        mysep =""
+        return  float(mysep.join(x))
 
 
 class ops(object):  #checking for the signs +-/abs etc
     def check_for_ops(self, lnum, rnum, ssign): # for equations that have different side for example plus minus divide
-        if (check_str_contains_list(lnum, ops().lreserv()) == False) and (check_str_contains_list(rnum, ops().lreserv()) == False):
-            return [float(lnum),float(rnum)]
+        if(len(lnum) == 0):
+            lnum = 0
+        if(len(ssign)==0):
+                
+            if (check_str_contains_list(lnum, ops().lreserv()) == False) and (check_str_contains_list(rnum, ops().lreserv()) == False):
+                return [float(lnum),float(rnum)]
+            else:
+                    if (check_str_contains_list(lnum, ops().lreserv()) == True):
+                        lnum = ops().numloop(lnum)
+                    if (check_str_contains_list(rnum, ops().lreserv()) == True):
+                        rnum = ops().numloop(rnum)
+                    return [float(lnum), float(rnum)]
         else:
-                if (check_str_contains_list(lnum, ops().lreserv()) == True):
-                    lnum = ops().numloop(lnum)
-                if (check_str_contains_list(rnum, ops().lreserv()) == True):
-                    rnum = ops().numloop(lnum)
-                return [float(lnum), float(rnum)]
+            if (check_str_contains_list(lnum, ops().lreserv()) == False) and (check_str_contains_list(rnum, ops().lreserv()) == False) and (check_str_contains_list(ssign, ops().lreserv()) == False):
+                return [str(lnum), str(rnum),float(ssign)]
+            else:
+                    if (check_str_contains_list(lnum, ops().lreserv()) == True):
+                        lnum = ops().numloop(lnum)
+                    if (check_str_contains_list(rnum, ops().lreserv()) == True):
+                        rnum = ops().numloop(rnum)
+                    if (check_str_contains_list(ssign, ops().lreserv()) == True) :
+                        ssign = ops().numloop(ssign)
+                    return [float(lnum), float(rnum), float(ssign)]
 
     def lreserv(self):
-        x = ["x", "y", "/", "+", "-", "*"]
+        x = ["x", "y", "/", "+", "-", "*","|"]
         return x
     
 
     def numloop(self, ssum):
-        def sides(ipos,ssum):
+        def sides(ipos,ssum): # for side operators like + - divide
             leftside = ssum[0:ipos]
             rightside= ssum[ipos+1: len(ssum)]
             return [leftside, rightside]
+
+             
+        def inop(ipos1, ipos2,ssum): # for inoperators such as sin cos || ln log
+            leftside = ssum[0:ipos1]
+            inside = ssum[ipos1+1:ipos2]
+            rightside = ssum[ipos2+1:len(ssum)]
+            return [leftside,rightside,inside]
         def switch(op_char, ipos, ssum):
+            ssum = str(ssum)
             if(op_char=="*"):
                 return formulas().multi(sides(ipos, ssum))
             elif (op_char == "/"):
@@ -72,6 +106,8 @@ class ops(object):  #checking for the signs +-/abs etc
                 return formulas().plus(sides(ipos,ssum))
             elif (op_char == "-"):
                 return formulas().minus(sides(ipos, ssum))
+            elif (op_char =="|"):
+                return formulas().abs(inop(ipos,ssum.find(op_char,ipos+1,len(ssum)),ssum))
 
         for k in ssum:
             if check_str_contains_list(ssum, ops().lreserv())==False:
