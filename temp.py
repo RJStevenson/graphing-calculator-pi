f84b187337d1be2a0eafb1cbdd7a940c90699524
James Stevenson <james.steve.dev@gmail.com>
2020-07-08 22:33:18 +0200

    got bracket mutliplication working

--- temp.py
+++ temp.py
@@ -20,12 +20,23 @@ class ops(object):
         stemp =""
         ipos1 = -1
         ipos2= -1
+        ibrac = -1
         for k in ssum:
             if k =="(":
+                if ssum.index(k) != 0:
+                    ibrac = ssum.index(k) -1
+
+                    if ssum[ibrac] not in ops.reg_operators:
+                     ssum = ssum[0:ibrac +1] + "*" + ssum[ibrac +1:len(ssum)]
+
                 if ipos1 ==-1:
                     ipos1 == ssum.index(k)
                 ops.op_count[0] = ops.op_count[0] +1
             if k ==")":
+                if ssum.index(k) != len(ssum)-1:
+                    ibrac = ssum.index(k) + 1
+                    if ssum[ibrac] not in ops.reg_operators:
+                     ssum = ssum[0:ibrac ] + "*" + ssum[ibrac:len(ssum)]
                 ops.op_count[0] = ops.op_count[0] -1
                 if (ops.op_count[0]==0) and (ipos1!=-1):
                     ipos2 = ssum.index(k)
@@ -34,5 +45,6 @@ class ops(object):
                     ssum.replace(ssum[ipos1: ipos2 + 1], inop(ssum[ipos1+1:ipos2]))
         return eval(ssum)
 sdata = input("enter your equation: ")
+sdata = str(sdata).replace(" ", "")
 sdata = ops().numloop(sdata)
 print(str(sdata))
